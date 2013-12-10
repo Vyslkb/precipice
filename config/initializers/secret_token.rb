0e35e6bda2494489318a1fb0838508d212054cde
James Holst <jamesmholst@yahoo.com>
2013-12-10 14:27:01 +0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Precipice::Application.config.secret_key_base = '87d276f46b2c5abbb162e8d786e8f49e02d93aef6fb48d0ddc76f6c71120e1f1181406729be958f36465d10b73f8bda302b2eb6a4abfe9a80f0868f0f52f5f0a'
