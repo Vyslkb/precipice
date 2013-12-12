4fd3a2eb474dd4e6a6172b8ff1836614bbf1e6c5
James Holst <jamesmholst@yahoo.com>
2013-12-13 02:38:39 +0700

    asd

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,5 +1,7 @@
 class Collection < ActiveRecord::Base
-  has_many :galleries, :inverse_of => :collection 
+  has_many :galleries, :inverse_of => :collection, :dependent => :restrict_with_error
+  
+  validates_presence_of :name 
 
 end
 
--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,6 +1,10 @@
 class Gallery < ActiveRecord::Base
   belongs_to :collection, :inverse_of => :galleries
-  has_many :photos, :inverse_of => :gallery
+  has_many :photos, :inverse_of => :gallery, :dependent => :restrict_with_error
+  
+  ### Validations
+  validates_presence_of :collection_id, :name
+  
   
   def name_with_collection
     "#{self.collection.name} - #{name}"
