7f6069ecc010508d4788891425ca3b6b00f6aefd
James Holst <jamesmholst@yahoo.com>
2013-12-19 02:10:34 +0700

    a

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,5 +25,9 @@ Precipice::Application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config.assets.debug = true
+  config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+   config.assets.js_compressor = :uglifier
+   config.assets.digest = true
+  config.assets.debug = false #changed for test
+  config.assets.compile = false #added fro test
 end
