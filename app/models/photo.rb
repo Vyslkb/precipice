43c273c46f39c4526bc691921eded06f3d78bb44
James Holst <jamesmholst@yahoo.com>
2014-01-06 21:51:19 +0700

    a

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,10 +2,12 @@ class Photo < ActiveRecord::Base
    ### Custom Attributes
    has_attached_file :photo_file, :styles => { :medium => "300x300>", 
                                                :thumb => "100x100>", 
-                                               :large => "1100x1100>" },
+                                               :large => "1100x1100>",
+                                               :original => "1400x1400>" },
                                                :convert_options => {:thumb => "-quality 60 -strip",
                                                                     :medium => "-quality 60 -strip",
-                                                                    :large => "-quality 60 -strip" }, 
+                                                                    :large => "-quality 60 -strip",
+                                                                    :original => "-quality 75" }, 
                                                :default_url => "/images/:style/missing.png"
    
   ### Associations
