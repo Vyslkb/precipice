0e62b4e013cf524b5656578add277ebf8c11d8a5
James Holst <jamesmholst@yahoo.com>
2014-01-23 00:19:50 +0630

    a

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -7,7 +7,7 @@ class AdminMailer < ActionMailer::Base
   
   def order_confirmation(customer_order)
     @customer_order = customer_order 
-    mail(to: 'jamesmholst@yahoo.com', subject: 'Thank you for your order from Beyond The Precipice')
+    mail(to: 'jamesmholst@yahoo.com, troutsnifferx@yahoo.com', subject: 'Thank you for your order from Beyond The Precipice')
   end
   
 end
