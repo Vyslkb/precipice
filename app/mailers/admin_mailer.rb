4cb15b6db042583e3e5a02b969ecbc682225ce01
James Holst <jamesmholst@yahoo.com>
2014-02-19 01:18:49 +0800

    d

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -17,10 +17,10 @@ class AdminMailer < ActionMailer::Base
   def order_confirmation(customer_order)
     @customer_order = customer_order 
     
-    tmp_file = open(SitePhoto.find_by_name('site_logo').photo_file.url(:large)).tempfile
-    attachments.inline['logo.png'] = File.read(tmp_file)
-    tmp_file.close
-    tmp_file.unlink   # deletes the temp file
+    #tmp_file = open(SitePhoto.find_by_name('site_logo').photo_file.url(:large)).tempfile
+    #attachments.inline['logo.png'] = File.read(tmp_file)
+    #tmp_file.close
+    #tmp_file.unlink   # deletes the temp file
     
     
     mail(to: 'jamesmholst@yahoo.com', subject: 'Thank you for your order from Beyond The Precipice')
