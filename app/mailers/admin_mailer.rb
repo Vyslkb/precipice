8b37b219f8bdf809034350ae805190ac7cc650d8
James Holst <jamesmholst@yahoo.com>
2014-02-19 00:51:14 +0800

    d

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -4,12 +4,12 @@ class AdminMailer < ActionMailer::Base
   default from: "Michael@BeyondThePrecipice.com"
   
   def welcome_email  
-    tmp_file = open(SitePhoto.find_by_name("site_logo").photo_file.url(:original))
+    #tmp_file = open(SitePhoto.find_by_name("site_logo").photo_file.url(:original))
     
     #SitePhoto.find_by_name("site_logo").photo_file.url(:original))
-    attachments.inline['logo.png'] = File.read(tmp_file)
-    tmp_file.close
-    tmp_file.unlink   # deletes the temp file
+    #attachments.inline['logo.png'] = File.read(tmp_file)
+    #tmp_file.close
+    #tmp_file.unlink   # deletes the temp file
     
     mail(to: 'jamesmholst@yahoo.com', subject: 'Testing Email Solution')
   end
